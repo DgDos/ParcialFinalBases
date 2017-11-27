a2eb4e1b5b6b491dd77596abf08f6d455cc22076
-948 <-948@5155004-PC103>
2017-11-27 13:16:18 -0500

    util

--- src/main/java/util/DbUtil.java
+++ src/main/java/util/DbUtil.java
@@ -2,6 +2,8 @@ package util;
 
 import java.io.IOException;
 import java.io.InputStream;
+import java.net.URI;
+import java.net.URISyntaxException;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
@@ -11,23 +13,26 @@ public class DbUtil {
 
     private static Connection connection = null;
 
-    public static Connection getConnection() {
+    public static Connection getConnection() throws URISyntaxException {
         if (connection != null) {
             return connection;
         } else {
-            try {
-                Properties prop = new Properties();
-                InputStream inputStream = DbUtil.class.getClassLoader().getResourceAsStream("dbProperties/db.properties");
-                prop.load(inputStream);
-                String driver = prop.getProperty("driver");
-                String url = prop.getProperty("url");
-                String user = prop.getProperty("user");
-                String password = prop.getProperty("password");
-                Class.forName(driver);
-                connection = DriverManager.getConnection(url, user, password);
-            } catch (ClassNotFoundException | SQLException | IOException e) {
+            URI dbUri = new URI(System.getenv("DATABASE_URL"));
+            String username = dbUri.getUserInfo().split(":")[0];
+            String password = dbUri.getUserInfo().split(":")[1];
+            String dbUrl = "jdbc:postgresql://" + dbUri.getHost() + ':' + dbUri.getPort() + dbUri.getPath();
+
+            if (connection == null) {
+                try {
+                    connection = DriverManager.getConnection(dbUrl, username, password);
+                } catch (SQLException e) {
+                    System.out.println("Connection Failed! Check output console");
+                    e.printStackTrace();
+                }
+
             }
             return connection;
+
         }
 
     }
